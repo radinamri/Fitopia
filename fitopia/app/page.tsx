25a629ede2fe9042d48398bae0f9ae0045966bf9
Radin Amri <radin.amrihesari@gmail.com>
2025-03-15 15:02:18 +0330

    feat: complete landing page

--- fitopia/app/page.tsx
+++ fitopia/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         loop
         muted
       />
-      <div className="absolute top-64 left-0 right-0 flex flex-col items-center justify-center gap-8 z-20">
+      <div className="absolute top-50 left-0 right-0 flex flex-col items-center justify-center gap-8 z-20">
         <div className="flex flex-col justify-center items-center">
           <p className="font-bold text-4xl text-white">
             TRY ON The Future of Fashion
@@ -24,14 +24,14 @@ export default function Home() {
         </div>
         <div className="flex flex-row justify-center items-center gap-16">
           <Link
-            className="flex flex-row justify-center items-center font-medium text-lg text-white hover:underline hover:underline-offset-4 transition-colors duration-200 gap-1"
+            className="flex flex-row justify-center items-center font-medium text-md text-white hover:underline hover:underline-offset-4 transition-colors duration-200 gap-1"
             href={"/"}
           >
             <PersonStanding />
             Create Avatar
           </Link>
           <Link
-            className="flex flex-row justify-center items-center font-medium text-lg text-white hover:underline hover:underline-offset-4 transition-colors duration-200 gap-1"
+            className="flex flex-row justify-center items-center font-medium text-md text-white hover:underline hover:underline-offset-4 transition-colors duration-200 gap-1"
             href={"/"}
           >
             <Camera />
@@ -41,7 +41,7 @@ export default function Home() {
       </div>
       <div className="flex flex-col w-[85%] h-auto justify-center items-center bg-white rounded-4xl shadow-xl p-16 gap-8">
         <div className="flex flex-row justify-between items-center">
-          <div className="flex flex-col justify-center items-start gap-16">
+          <div className="flex flex-col justify-center items-start gap-8">
             <div>
               <p className="font-bold text-4xl dark:text-[#171717]">
                 TRY ON The Future of Fashion
@@ -51,12 +51,6 @@ export default function Home() {
               </p>
             </div>
             <div className="flex flex-row w-full justify-start items-center gap-8">
-              <Link
-                className="border-2 border-[#171717] rounded-xl p-2 pl-4 pr-4 font-bold text-md dark:text-[#171717] hover:text-white hover:bg-[#171717] transition-colors duration-200"
-                href={"/try-on"}
-              >
-                Virtual Try On
-              </Link>
               <Link
                 className="font-medium text-md dark:text-[#171717] hover:underline hover:underline-offset-4 transition-colors duration-200"
                 href={"/about"}
